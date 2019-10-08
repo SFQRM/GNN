0b27b55100b195d96ec988f067ec6aae3259878a
SFQRM <zhuojinghan@126.com>
2019-10-08 12:26:42 +0800

    Update Zachary’s_Karate_Club.py

--- Zachary’s_Karate_Club.py
+++ Zachary’s_Karate_Club.py
@@ -9,6 +9,19 @@
 
 
 import numpy as np
+from networkx import karate_club_graph, to_numpy_matrix
 
+zkc = karate_club_graph()                                                       # zkc:数据集
+order = sorted(list(zkc.nodes()))                                               # 排序
+# print(sorted(list(zkc.nodes())))
 
+"""
+    to_numpy_matrix(G, nodelist=None, dtype=None, order=None, multigraph_weight=<built-in function sum>, weight='weight')
+    功能：以NumPy矩阵的形式返回图邻接矩阵。
+"""
+A = to_numpy_matrix(zkc, nodelist=order)                                        #
+I = np.eye(zkc.number_of_nodes())
+print(A)
+print(zkc.number_of_nodes())
+print(I)
 
