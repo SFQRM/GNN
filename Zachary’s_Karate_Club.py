155caa2d1639e6d7c1712089e0db3e012dca20e6
SFQRM <zhuojinghan@126.com>
2019-10-08 11:53:18 +0800

    Create Zachary’s_Karate_Club.py

---
+++ Zachary’s_Karate_Club.py
@@ -0,0 +1,14 @@
+# @author: SFQRM
+# first edit time = 2019-10-8
+# function:
+#    Zachary’s karate club is a commonly used social network where nodes represent members of
+# a karate club and the edges their mutual relations. While Zachary was studying the karate club,
+# a conflict arose between the administrator and the instructor which resulted in the club splitting in two.
+# The code below shows the graph representation of the network and nodes are labeled according to
+# which part of the club. The administrator and instructor are marked with ‘A’ and ‘I’, respectively.
+
+
+import numpy as np
+
+
+
